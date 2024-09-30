e415a6733e8e0147fd410e59bf75725a78fd8421
Nguyenquangtruong25 <167264349+Nguyenquangtruong25@users.noreply.github.com>
2024-09-30 14:09:38 +0700

    Add files via upload

---
+++ Operator/js/main.js
@@ -0,0 +1,20 @@
+var a = Number(prompt("nhap a"))
+var b = Number(prompt("nhap a"))
+var tong = a + b
+var hieu = a - b
+var tich = a * b
+var thuong = a / b
+document.write("tong"+tong + "<br>")
+document.write("hieu"+hieu + "<br>")
+document.write("tich"+tich + "<br>")
+document.write("thuong"+thuong + "<br>")
+
+document.write("************************<br>")
+var c = a > b
+document.write(a + ">"+ b + "=" + c + "<br")
+var c = a < b
+document.write(a + "<"+ b + "=" + c + "<br")
+var c = a >= b
+document.write(a + ">="+ b + "=" + c + "<br")
+var c = a => b
+document.write(a + "<="+ b + "=" + c + "<br")
